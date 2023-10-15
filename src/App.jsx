9cd1a33cd5b039e23ac75fce5ff929d4acd47b98
imelshety <mohamedkingman99@gmail.com>
2023-10-15 17:20:14 +0300

    add Product Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Hero from "./components/Hero";
+import Products from "./components/Products";
 import Services from "./components/Services";
 
 export default function App() {
@@ -6,6 +7,7 @@ export default function App() {
     <>
       <Hero />
       <Services />
+      <Products />
     </>
   )
 }
